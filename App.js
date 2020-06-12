bf63639eb1aeacb90089d91c0ed1ebb25c4498a2
j-dublon <j.johnson.dublon@gmail.com>
2020-06-12 13:48:27 +0100

    basic app setup

--- App.js
+++ App.js
@@ -1,79 +1,8 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  Alert,
-  TouchableWithoutFeedback,
-  Keyboard,
-} from "react-native";
-import Header from "./components/Header";
-import ToDoItem from "./components/ToDoItem";
-import AddToDo from "./components/AddToDo";
-import Sandbox from "./components/SandBox";
+import Home from "./screens/Home";
 
 export default function App() {
-  const [todos, setTodos] = useState([
-    { text: "buy coffee", key: "1" },
-    { text: "create an app", key: "2" },
-    { text: "play on the switch", key: "3" },
-  ]);
-
-  const pressHandler = (key) => {
-    setTodos((currentToDos) => {
-      return currentToDos.filter((todo) => todo.key !== key);
-    });
-  };
-
-  const submitHandler = (text) => {
-    if (text.length > 3) {
-      setTodos((currentToDos) => {
-        return [{ text: text, key: Math.random().toString() }, ...currentToDos];
-      });
-    } else {
-      Alert.alert("OOPS!", "ToDos must be more than 3 characters long", [
-        { text: "Understood", onPress: () => console.log("Alert closed") },
-      ]);
-    }
-  };
-
-  return (
-    // <Sandbox />
-    <TouchableWithoutFeedback
-      onPress={() => {
-        Keyboard.dismiss();
-      }}
-    >
-      <View style={styles.container}>
-        <Header />
-        <View style={styles.content}>
-          <AddToDo submitHandler={submitHandler} />
-          <View style={styles.list}>
-            <FlatList
-              data={todos}
-              renderItem={({ item }) => (
-                <ToDoItem item={item} pressHandler={pressHandler} />
-              )}
-            />
-          </View>
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
-  );
+  return <Home />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  content: {
-    padding: 40,
-    flex: 1,
-  },
-  list: {
-    marginTop: 20,
-    flex: 1,
-  },
-});
+const styles = StyleSheet.create({});
